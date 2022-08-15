d45109a6e1daa1a2cbcd8d0feb9e10013e6dd074
annemarierolo <annemarierolo@gmail.com>
2022-08-15 14:50:31 +0500

    Adding mongodb cloud and heroku

--- app.js
+++ app.js
@@ -1,48 +1,134 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
 const port = 3000;
-
-let items = ['hola', 'chao', 'como', 'estas'];
-let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+mongoose.connect("mongodb+srv://admin:YsTexFzeD6Qn1wEW@cluster0.jnwds4t.mongodb.net/todolistDB", { useNewUrlParser: true })
 
-    let today = new Date();
-    let currentDay = today.getDay();
+const itemSchema = {
+    name: String
+};
 
+const Item = mongoose.model("Item", itemSchema);
 
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
-
-    let day = today.toLocaleDateString("en-US", options);
-    res.render("list", { listTitle: day, newItems: items });
+const item1 = new Item({
+    name: "Welcome to todolist",
 });
 
+const item2 = new Item({
+    name: "Hit the + button to add new item",
+});
+
+const item3 = new Item({
+    name: "<- Hit this to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
+
+//Adding default items to list and print it on todolist
+app.get('/', (req, res) => {
+
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Succesfully saved all the items to todolistDB");
+
+                }
+            });
+            res.redirect('/');
+        } else {
+            res.render("list", { listTitle: "Today", newItems: foundItems });
+        }
+    });
+
+
+});
+
+app.get('/:customListName', (req, res) => {
+
+    const customListName = _.capitalize(req.params.customListName);
+    List.findOne({ name: customListName }, (err, foundList) => {
+        if (!err) {
+            if (!foundList) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                })
+
+                list.save();
+                res.redirect('/' + customListName);
+            } else {
+                res.render("list", { listTitle: foundList.name, newItems: foundList.items });
+            }
+        }
+    })
+
+});
+
+
+
+//Adding new item to mongodb and print it on todolist 
 app.post('/', (req, res) => {
-    let item = req.body.itemList;
+    const itemName = req.body.itemList;
+    const listName = req.body.list;
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('/work');
+    const item = new Item({
+        name: itemName
+    })
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect('/');
     } else {
-        items.push(item);
-        res.redirect("/");
+        List.findOne({ name: listName }, (err, foundList) => {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect('/' + listName);
+        })
     }
-
-
 })
 
-app.get('/work', (req, res) => {
-    res.render("list", { listTitle: "Work List", newItems: workItems });
+
+
+app.post('/delete', (req, res) => {
+    const itemCheckedId = req.body.checkedbox;
+    const listName = req.body.listItem;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(itemCheckedId, (err) => {
+            if (!err) {
+                console.log("Succesfully deleted the document");
+                res.redirect('/');
+            }
+        });
+    } else {
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: itemCheckedId } } }, (err, foundList) => {
+            if (!err) {
+                console.log(listName);
+
+                res.redirect('/' + listName);
+            }
+        })
+    }
+
 })
 
 app.get('/about', (req, res) => {
